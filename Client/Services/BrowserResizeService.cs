6af395c6afb9a4314a47cd1d1811db7c0128a777
boredanyway <ben.strohmann@gmail.com>
2020-07-21 11:45:56 +0200

    client services added

---
+++ Client/Services/BrowserResizeService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.JSInterop;
+using Oqtane.Modules;
+using Oqtane.Services;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Oqtane.ChatHubs.Services
+{
+    public class BrowserResizeService : ServiceBase, IService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public static event Func<Task> OnResize;
+
+        public BrowserResizeService(HttpClient http, IJSRuntime jsRuntime) : base(http)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        [JSInvokable("OnBrowserResize")]
+        public static async Task OnBrowserResize()
+        {
+            await OnResize?.Invoke();
+        }
+
+        public async Task<int> GetInnerHeight()
+        {
+            return await _jsRuntime.InvokeAsync<int>("browserResize.getInnerHeight");
+        }
+
+        public async Task<int> GetInnerWidth()
+        {
+            return await _jsRuntime.InvokeAsync<int>("browserResize.getInnerWidth");
+        }
+    }
+}
---
+++ Client/Services/ScrollService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+using Oqtane.Modules;
+using Oqtane.Services;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Oqtane.ChatHubs.Services
+{
+    public class ScrollService : ServiceBase, IService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public ScrollService(HttpClient http, IJSRuntime jsRuntime) : base(http)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task ScrollToBottom(string element, int time)
+        {
+            await _jsRuntime.InvokeAsync<object>("scroll.scrollToBottom", element, time);
+        }
+
+    }
+}
